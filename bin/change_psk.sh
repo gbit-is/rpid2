1a52412c8cb18e49a17ffcb0dfba831d23c4213a
Guðrún Ólafsdóttir <Gudrun.Olafsdottir@landsbankinn.is>
2025-08-15 07:33:49 +0000

    don't hardcode the debug location

--- bin/change_psk.sh
+++ bin/change_psk.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 config_file="/etc/hostapd/hostapd.conf"
-config_file="/Users/gudrun/Documents/GitHub/rpid2/bin/hostapd.conf"
+config_file="$SCRIPT_DIR/hostapd.conf"
 
 current_line=$(grep "wpa_passphrase" $config_file)
 current_pass=$(echo "$current_line" | awk -F '=' '{print $2}')
