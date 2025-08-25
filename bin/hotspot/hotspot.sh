3dd33c33fe0810981bd9ec6d5ac25a50e9ec3375
gbit-is <gbit@gbit.is>
2025-08-25 11:44:57 +0100

    fix hotspot script

--- bin/hotspot/hotspot.sh
+++ bin/hotspot/hotspot.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#set -x
+
+
+wifi_check_max=10
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
@@ -8,38 +12,68 @@ if [[ "$(whoami)" != "root" ]];then
 	exit 1
 fi
 
-# eligable interfaces for being connected
 
 cd ..
 source ../venv/bin/activate
 
 
-
-interfaces="eth0 wlan0"
-interfaces=$(./list_configs.py  network_config interfaces)
-interfaces=$(echo "$interfaces" | sed 's/,/ /g')
-echo "$interfaces"
-states=""
-
-# for each eligable interface
-for interface in $interfaces;do
-	# check the state of the interface, add to "states"
-	state=$(ip -json addr show $interface | jq '.[].operstate' -r)
-	states="${states}${state}\n"
-done
-
-#clean up list due to dirty way of generating said list
-states=$(echo -e "$states" | grep .)
-
-# check if any eligable interface is up
-echo "$states" | grep "UP" > /dev/null
+rfkill list | grep "Wireless" -A2 | grep "blocked" | grep yes > /dev/null
 if [ $? -eq 0 ];then
-	echo "a network interface is up"
-	echo "no need to continue"
-	#exit 0
-else
-	echo "no network interface is up"
+	echo "wifi is being softblocked, unblocking wifi"
+        rfkill unblock wifi
 fi
+
+nmcli radio | grep "disabled" > /dev/null
+if [ $? -eq 0 ];then
+	echo "wifi is disabled in nmcli,enabling it"
+	#nmcli radio wifi on
+fi
+
+
+
+
+has_wifi="False"
+try_wifi="True"
+wifi_checks=0
+
+while [[ "$try_wifi" == "True" ]];do
+	# eligable interfaces for being connected
+	interfaces=$(./list_configs.py  network_config interfaces)
+	interfaces=$(echo "$interfaces" | sed 's/,/ /g')
+	states=""
+
+	# for each eligable interface
+	for interface in $interfaces;do
+		# check the state of the interface, add to "states"
+		state=$(ip -json addr show $interface | jq '.[].operstate' -r)
+		states="${states}${state}\n"
+	done
+
+	#clean up list due to dirty way of generating said list
+	states=$(echo -e "$states" | grep .)
+
+	# check if any eligable interface is up
+	echo "$states" | grep "UP" > /dev/null
+	if [ $? -eq 0 ];then
+		echo "a network interface is up"
+		echo "no need to continue"
+		exit 0 
+		try_wifi="False"
+		has_wifi="True"
+	else
+		echo "no network interface is up"
+	fi
+
+	wifi_checks=$((wifi_checks + 1))
+
+	if [ $wifi_checks -gt $wifi_check_max ];then
+		echo "given up on finding wifi"
+		try_wifi="False"
+	fi
+
+	sleep 1
+
+done
 
 
 cd $SCRIPT_DIR
@@ -47,16 +81,37 @@ cd $SCRIPT_DIR
 ./edit_hotspot.sh
 
 
+
+
 echo "stopping onboard WIFI and starting hotspot"
 
-sudo nmcli radio wifi off
+#sudo nmcli radio wifi off
 sudo pkill -x wpa_supplicant || true
 sleep 1
 sudo pkill -9 -x wpa_supplicant || true
 sudo ip link set wlan0 down
 sudo ip addr flush dev wlan0
+
+
+
+
+rfkill list | grep "Wireless" -A2 | grep "blocked" | grep yes > /dev/null
+if [ $? -eq 0 ];then
+	rfkill unblock wifi
+fi
+
+
+nmcli radio | grep "disabled" > /dev/null
+if [ $? -eq 0 ];then
+	echo "wifi is disabled in nmcli,enabling it"
+	#nmcli radio wifi on
+fi
+
+
 sudo ip link set wlan0 up
 sudo ip addr add 192.168.4.1/24 dev wlan0
 sudo systemctl start dnsmasq
 sudo systemctl start hostapd
+
+
 
