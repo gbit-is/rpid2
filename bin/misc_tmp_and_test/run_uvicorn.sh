eef1cad221a9adfa4ade7bcce93318ba3c7a2409
gbit-is <gbit@gbit.is>
2025-08-28 12:11:58 +0100

    name change

--- bin/misc_tmp_and_test/run_uvicorn.sh
+++ bin/misc_tmp_and_test/run_uvicorn.sh
@@ -1,4 +1,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 cd ..
-uvicorn web_server:app --reload --port 9000 --host 0.0.0.0
+uvicorn api_server:app --reload --port 9000 --host 0.0.0.0
